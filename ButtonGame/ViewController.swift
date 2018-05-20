7eeed91bac030a5f373cb983ea7d5f3787834bd6
MosesKing <mkc42@mail.missouri.edu>
2018-05-19 19:37:01 -0500

    Initial Commit

---
+++ ButtonGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ButtonGame
+//
+//  Created by moe on 5/19/18.
+//  Copyright © 2018 m&s. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
